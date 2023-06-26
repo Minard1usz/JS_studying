3d1e54369e0a3943dfd31fe03feccb4cb9723c13
Yevhen Larin <minard1usz@gmail.com>
2023-06-26 12:47:30 +0300

    hw3

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let a = prompt("Введіть перше число", '');
+let b = prompt("Введіть друге число", '');
+let c = parseInt(a) + parseInt(b);
+let d = parseInt(a) - parseInt(b)
+let e = parseInt(a) * parseInt(b);
+let f = parseInt(a) / parseInt(b);
+alert('Користувач ввів ' + `${a}` + ' і ' + `${b}` + ' : ' + ' \n' + `${a}` + ' + ' + `${b}` + ' = ' + `${c}` + ' \n' + `${a}` + ' - ' + `${b}` + ' = ' + `${d}` + ' \n' + `${a}` + ' * ' + `${b}` + ' = ' + `${e}` + ' \n' + `${a}` + ' / ' + `${b}` + ' = ' + `${f}`);
