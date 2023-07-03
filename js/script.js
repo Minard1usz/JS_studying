1763ebed5febd97f9ab1caa4cd6d9dd1dea3a61f
Yevhen Larin <minard1usz@gmail.com>
2023-07-03 13:22:08 +0300

    hw6

--- js/script.js
+++ js/script.js
@@ -1,117 +1,64 @@
-// // 1. Вивести на сторінку в один рядок через кому числа від 10 до 20.
-let num = 10;
-while (num <= 20) {
-    document.write(num + ', ');     
-    num++;
+// // 1. Вивести числа від 20 до 30 через пропуск, використовуючи крок 0,5 (20 20,5 21 21,5….).
+let num = 20;
+let half = 0.5;
+for(; num <= 30; num = num + half) {
+    console.log(num);
 }
-document.write('<br>')
-document.write('<br>')
 
-// // або
-// for(let number = 10; number <= 20; number = number + 1)
-// console.log(number);
+// // 2. Один долар коштує 27 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів.
+let rate = 27;
+let total = 10;
+let sum;
+for(total; total <= 100; total = total + 10) {
+    sum = total / rate;
+    console.log(sum.toFixed(2));
+}
 
-// // 2. Вивести квадрати чисел від 10 до 20.
-let num2 = 10;
+// 3. Дане ціле число. Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N.
+let n = prompt('Введіть ціле число');
+let i = 1;
+let sum3;
+for(i; i <= 100; i++) {
+    if(sum3 = i * i <= n) {
+    console.log(i); }
+}
 
-for (; num2 <= 20; num2++) {
-    if (num2) {
-        console.log(num2**2);
+// 4. Дане ціле число. З'ясувати, чи є воно простим (простим називається число, більше 1, які не мають інших дільників крім 1 і себе).
+// let n = prompt('Введіть ціле число аби отримати відповідь, просте воно чи складене');
+
+let number = parseInt(prompt('Введіть ціле число аби отримати відповідь, просте воно чи складене'));
+let isPrime = true;
+
+if (number === 1) {
+    alert('1 не є ні простим, ні складеним числом')
+    console.log('1 не є ні простим, ні складеним числом')
+}   else if (number > 1) {
+    for (let i = 2; i < number; i++) {
+        if (number % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        alert(`${number} це просте число`);
+        console.log(`${number} це просте число`);
+    } else {
+        alert(`${number} це складене число`);
+        console.log(`${number} це складене число`);
     }
 }
 
-// //3. Вивести таблицю множення на 7
-let num3 = 1;
-let multiplier = 7;
-for(; num3 <= 10; num3++) {
-    if (num3) {
-        console.log(num3*multiplier);
+// 5. Дане деяке число. Визначити, чи можна одержати це число шляхом зведення числа 3 у деякий ступінь. (Наприклад, числа 9, 81 можна отримати, а 13 - не можна).
+let number = parseInt(prompt('Введіть число '));
+let result;
+
+while (number % 3 == 0) {
+       result = number /= 3;
+       console.log(result);     
+    } if (result == 1) {
+        console.log('possible');
+    } else {
+        console.log('impossible');
     }
-}
 
-// //4. Знайти суму всіх цілих чисел від 1 до 15.
-let sum4 = 0;
-for(let num4 = 0; num4 <= 15; num4++) {
-    sum4 = sum4 + num4;
-}
-console.log(sum4);
-
-// //5. Знайти добуток усіх цілих чисел від 15 до 35.
-let mult5 = 1;
-for(let num5 = 15; num5 <= 35; num5++) {
-    mult5 = mult5*num5;
-}
-console.log(mult5);
-
-// //6. Знайти середнє арифметичне всіх цілих чисел від 1 до 500.
-let sum6 = 0;
-let divider6 = 2;
-let result6;
-
-for(let num6 = 1; num6 <= 500; num6++) {
-    sum6 = sum6 + num6;
-    result6 = sum6 / divider6
-}
-console.log(result6);
-
-// //7. Вивести суму лише парних чисел в діапазоні від 30 до 80.
-let num7 = 30;
-let sum7 = 0;
-
-for(; num7 <= 80; num7 = num7 + 2) {
-    sum7 = sum7 + num7;
-}
-console.log(sum7);
-
-// //8. Вивести всі числа в діапазоні від 100 до 200 кратні 3.
-let num8 = 100;
-
-for(; num8 <= 200; num8++) {
-    if (num8 % 3 === 0) {
-        console.log(num8);
-    }
-}
-
-//9. Дано натуральне число. Знайти та вивести на сторінку всі його дільники.
-let n = +prompt('Введіть натуральне число');
-
-for (i = 1; i <= n; i++) {
-    if (n % i == 0) {
-        alert(`Дільники вашого числа це: ${i}`);
-        console.log(i);
-        document.write(i + ', ');
-    }
-}
-
-//10. Визначити кількість його парних дільників.
-let n = +prompt('Введіть натуральне число');
-let divisorsNumber = 0;
-
-for (i = 0; i <= n; i = i + 2) {
-    if (n % i == 0) {
-        alert(`Парні дільники вашого числа це: ${i}`);
-        console.log(i);
-    } 
-}
-
-//11. Знайти суму його парних дільників.
-let n = +prompt('Введіть натуральне число');
-let sum11 = 0;
-
-for (i = 0; i <= n; i = i + 2) {
-    if (n % i == 0) {
-        alert(`Парні дільники вашого числа це: ${i}`);
-        console.log(i);
-        sum11 = sum11 + i;
-    }
-}
-console.log(sum11);
-
-//12. Надрукувати повну таблицю множення від 1 до 10.
-for (i = 1; i <= 10; i = i + 1) {
-    for (j = 1; j <= 10; j = j + 1) {
-        number = i * j;
-        document.write(' ' + number + ' ')
-    }
-    document.write('<br>')
-}
