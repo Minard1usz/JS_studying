bf4327f11d1e7a0e9c22183c75efda7fb712c632
Yevhen Larin <minard1usz@gmail.com>
2023-07-17 19:11:25 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,70 +1,19 @@
 
 
-alert('Будемо знаходить рішення квадратного рівняння виду:\nax^2 + bx + c = 0');
-    
+let myArray = ["Taylor Swift", "The Weekend", "Billie Eilish", "Rihanna", "Ariana Grande", "Bruno Mars", "Dua Lipa", "Ed Sheeran", "Lady Gaga", "Beyonce", "Miley Cyrus"];
 
-function promptUserForNumber() {
-
-    while (isNaN(numberOne)) {
-        alert('Це не число, будь ласка, введіть число');
-        numberOne = prompt('Введіть число "a"'); 
-    }   
-    while (isNaN(numberTwo)) {
-        alert('Це не число, будь ласка, введіть число');
-        numberTwo = prompt(`Користувач ввів число "a" = ${numberOne}` + ' \nВведіть число "b" ');
-    } 
-    while (isNaN(numberThree)) {
-        alert('Це не число, будь ласка, введіть число');
-        numberThree = prompt(`Користувач ввів число "a" = ${numberOne}` + `\nКористувач ввів число "b" = ${numberTwo}` + ' \nВведіть число "c" ');
-    } 
-    if (!isNaN(numberOne)) {
-        console.log(numberOne, numberTwo, numberThree);
-    } 
-
-    return alert(`Користува ввів число "a" = ${numberOne}` + `\nКористувач ввів число "b" = ${numberTwo}` + `\nКористувач ввів число "c" = ${numberThree}`), 
-    alert(quad(a = `${numberOne}`, b = `${numberTwo}`, c = `${numberThree}`)),
-    console.log(quad(a = `${numberOne}`, b = `${numberTwo}`, c = `${numberThree}`)); 
+function removeElement (array, element) {
+    array = myArray;
+    element = myArray.splice(element, 1);
+    console.log(myArray);
 }
 
-function checkForNull() {
+removeElement(myArray, 1);
 
-    numberOne = prompt('Введіть число "a" ');
-    if (numberOne === null || !numberOne) {
-        alert('Жаль. Сподіваюсь ще побачитись!');
-        console.log('Користувач відмовився вводити дані');
-        return numberOne;
-    } 
-    numberTwo = prompt(`Користувач ввів число "a" = ${numberOne}` + ' \nВведіть число "b" ');
-    if (numberTwo === null || !numberTwo) {
-        alert('Жаль. Сподіваюсь ще побачитись!');
-        console.log('Користувач відмовився вводити дані');
-        return numberTwo;
-    } 
-    numberThree = prompt(`Користувач ввів число "a" = ${numberOne}` + `\nКористувач ввів число "b" = ${numberTwo}` + ' \nВведіть число "c" ');
-    if (numberThree === null || !numberThree) {
-        alert('Жаль. Сподіваюсь ще побачитись!');
-        console.log('Користувач відмовився вводити дані');
-        return numberThree;
-    } 
-}
+// Пример аргументов функции
+// function calcSum(numOne, numTwo) {
+//     console.log(`Первая переменная : ${numOne}`);
+//     console.log(`Вторая переменная : ${numTwo}`);
+// }
 
-
-function quad(a, b, c) {
-    if(a == 0) return "a = 0. Рівняння не має рішень";
-    const D = b * b - 4 * a * c;
-    let x1, x2;
-    if(D > 0) {
-        x1 = (-b + Math.sqrt(D)) / (2 * a);
-        x2 = (-b - Math.sqrt(D)) / (2 * a);
-        return "Коефіцієнти: a = "+a+", b = "+b+", c = "+c+" \nКорені рівняння: X1 = " + x1 + ", X2 = " + x2 + "\nДискримінант(D) = " + D;
-    } else if (D == 0) {
-        x1 = -b / (2*a);
-        return "D = 0. Єдиний корень рівняння: " + x1 + "";
-    } else if (D < 0) {
-        return "D < 0. Рівняння не має рішень";
-    } return;
-} 
-
-
-promptUserForNumber(checkForNull());
-
+// calcSum(1, 2);
