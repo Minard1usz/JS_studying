c17b6636cf60c6ed1acbb7e6ab56d8b90031320e
Yevhen Larin <minard1usz@gmail.com>
2023-07-18 13:52:37 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
 
-
 let myArray = ["Taylor Swift", "The Weekend", "Billie Eilish", "Rihanna", "Ariana Grande", "Bruno Mars", "Dua Lipa", "Ed Sheeran", "Lady Gaga", "Beyonce", "Miley Cyrus"];
-
-function removeElement (array, element) {
+  function removeElement(array, element) {
     array = myArray;
     element = myArray.splice(element, 1);
-    console.log(myArray);
-}
-
-removeElement(myArray, 1);
-
-// Пример аргументов функции
-// function calcSum(numOne, numTwo) {
-//     console.log(`Первая переменная : ${numOne}`);
-//     console.log(`Вторая переменная : ${numTwo}`);
-// }
-
-// calcSum(1, 2);
+  console.log(myArray);
+    
+    for (let i = 1; i < myArray.length; i++) {
+    if (myArray[i] === element){
+      return i;
+    }
+  }
+  }
+  
+  
+  console.log(removeElement(myArray, 4));
+  
