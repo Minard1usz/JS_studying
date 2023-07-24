4b54698b4f822d77053baaafec819881ea5e9590
Yevhen Larin <minard1usz@gmail.com>
2023-07-25 00:40:43 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,14 +1,45 @@
-function sum(result) {
-    result = 0;
-    return function (elem) {
-        result += elem;
-        console.log(result);
+// Перший варіант
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this
+    },
+    down: function () {
+        this.step--;
+        return this
+
+    },
+    showStep: function () {
+        alert(this.step);
+        return this
+
     }
-}
+};
+
+ladder.up().up().down().showStep();
 
 
-let outcome = sum();
 
-outcome(3);
-outcome(5);
-outcome(20);
+
+
+// Другий варіант
+// let ladder = {
+//     step: 0,
+//     up: function () {
+//         this.step++;
+//         return this
+//     },
+//     down: function () {
+//         this.step--;
+//         return this
+//     },
+//     showStep: function () {
+//         alert(this.step);
+//         return this
+//     }
+// };
+
+// let result = ladder.up?.().up?.().down?.().showStep?.();
+
+// console.log(result);
