b002d88e5f0380fa51d2f832a5c89aa2926c9f1a
Yevhen Larin <minard1usz@gmail.com>
2023-08-02 09:02:16 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,45 +1,19 @@
-// Перший варіант
-let ladder = {
-    step: 0,
-    up: function () {
-        this.step++;
-        return this
-    },
-    down: function () {
-        this.step--;
-        return this
+let number1 = /380\s\d{2}\s\d{3}\s\d{2}\s\d{2}$/;
+let number2 = /\+380\s\d{2}\s\d{3}\s\d{2}\s\d{2}$/;
+let number3 = /380\-\d{2}\-\d{3}\-\d{2}\-\d{2}$/;
+let number4 = /\+380\-\d{2}\-\d{3}\-\d{2}\-\d{2}$/;
 
-    },
-    showStep: function () {
-        alert(this.step);
-        return this
-
+function validateData() {
+    let inputData = prompt('Enter your mobile starting with +380 / 380');
+    if (number1.test(inputData) || number2.test(inputData) || number3.test(inputData) || number4.test(inputData)) {
+        let res = inputData.replace(/\D/g, '')
+        res = res.replace(/^38/, '');
+        console.log(`your number is ${res}`);
+    }   
+    else if (!number1.test(inputData) || !number2.test(inputData) || !number3.test(inputData) || !number4.test(inputData)) {
+        console.log(null);
     }
-};
+    return inputData;   
+}
 
-ladder.up().up().down().showStep();
-
-
-
-
-
-// Другий варіант (стосується саме перевірки виклику)
-// let ladder = {
-//     step: 0,
-//     up: function () {
-//         this.step++;
-//         return this
-//     },
-//     down: function () {
-//         this.step--;
-//         return this
-//     },
-//     showStep: function () {
-//         alert(this.step);
-//         return this
-//     }
-// };
-
-// let result = ladder.up?.().up?.().down?.().showStep?.();
-
-// console.log(result);
+validateData();
