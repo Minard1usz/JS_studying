e1a4ad38313968b34e1644e4b8d3f4451bcb1057
Yevhen Larin <minard1usz@gmail.com>
2023-08-18 21:39:15 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,84 +1,182 @@
-class Hamburger {
-  constructor(size, stuffing, name) {
-    this.name = name;
-    this.size = size;
-    this.stuffing = stuffing;
-    this.toppings = [];
-  }
-  
-  addTopping(topping) {
-    this.toppings.push(topping);
-  }
-  
-  calculatePrice() {
-    const priceForSize = this.size.price;
-    const priceForStuffing = this.stuffing.price;
-    const priceForToppings = this.toppings.reduce((total, topping) => total + topping.price, 0);
-    return priceForSize + priceForStuffing + priceForToppings;
+class Student {
+  constructor(firstName, secondName, birthYear) {
+    this.firstName = firstName;
+    this.secondName = secondName;
+    this.birthYear = birthYear;
+    this.grades = [];
+    this.attendance = new Array(25).fill(undefined);
+    if (this.attendance.length > 25) {
+      console.log('too much');
+    }
   }
 
-  calculateCalories() {
-    const caloriesForSize = this.size.calories;
-    const caloriesForStuffing = this.stuffing.calories;
-    const caloriesForToppings = this.toppings.reduce((total, topping) => total + topping.calories, 0);
-    return caloriesForSize + caloriesForStuffing + caloriesForToppings;
+  getAge() {
+    const currentYear = new Date().getFullYear();
+    return currentYear - this.birthYear;
   }
+
+  addGrade(grade) {
+    this.grades.push(grade);
+  }
+
+  getAverageGrade() {
+    const calcAverageGrade = this.grades.reduce((total, grade) => total + grade, 0);
+    const sum = calcAverageGrade / this.grades.length;
+    return sum.toFixed(1);
+  }
+
+  present() {
+    const emptyItem = this.attendance.indexOf(undefined);
+    if (emptyItem !== -1) {
+      this.attendance[emptyItem] = true;
+    }
+  }
+
+  absent() {
+    const emptyItem = this.attendance.indexOf(undefined);
+    if (emptyItem !== -1) {
+      this.attendance[emptyItem] = false;
+    }
+  }
+
+  averageAttendance() {
+    const visitedSessions = this.attendance.filter(value => value == true).length;
+    const unVisitedSessions = this.attendance.filter(value => value == false).length;
+    const totalSessions = visitedSessions * 1 / (visitedSessions + unVisitedSessions);
+    return totalSessions.toFixed(1);
+  };
+
+  summary() {
+    const averageGrade = this.getAverageGrade();
+    const attendanceRate = this.averageAttendance();
+
+    if (averageGrade > 90 && attendanceRate > 0.9) {
+      return "Молодець!";
+    } else if (averageGrade > 90 || attendanceRate > 0.9) {
+      return "Добре, але можна краще";
+    } else {
+      return "Редиска!";
+    }
+  };
 }
 
-class ProductItem {
-  constructor(name, price, calories) {
-    this.name = name;
-    this.price = price;
-    this.calories = calories;
-  }
-}
-
-const SIZE_SMALL = new ProductItem('small', 50, 20);
-const SIZE_BIG = new ProductItem('big', 100, 40);
-
-const STUFFING_CHEESE = new ProductItem('cheese', 10, 20);
-const STUFFING_LETTUCE = new ProductItem('lettuce', 20, 5);
-const STUFFING_FRIES = new ProductItem('fries', 15, 10);
-
-const TOPPING_SPICE = new ProductItem('spice', 15, 0);
-const TOPPING_MAYO = new ProductItem('mayo', 20, 5);
 
 
-
-const hamburgerCheese = new Hamburger(SIZE_SMALL, STUFFING_CHEESE, 'the Spicy Hamburger');
-hamburgerCheese.addTopping(TOPPING_SPICE);
-console.log(hamburgerCheese);
-
-const hamCheeseTP = hamburgerCheese.calculatePrice();
-console.log(`Total price for ${hamburgerCheese.name} is ${hamCheeseTP}`);
-
-const hamCheeseCal = hamburgerCheese.calculateCalories();
-console.log(`Total calories for ${hamburgerCheese.name} is ${hamCheeseCal}`);
-
-
-
-
-const hamburgerMayo = new Hamburger(SIZE_BIG, STUFFING_FRIES, 'the Hamburger with Mayo');
-hamburgerMayo.addTopping(TOPPING_MAYO);
-console.log(hamburgerMayo);
-
-const hamMayoTP = hamburgerMayo.calculatePrice(); 
-console.log(`Total price for ${hamburgerMayo.name} is ${hamMayoTP}`)
-
-const hamMayoCal = hamburgerCheese.calculateCalories();
-console.log(`Total calories for ${hamburgerMayo.name} is ${hamMayoCal}`);
+const student1 = new Student('Jeremy', 'McAlister', 1990);
+student1.addGrade(99);
+student1.addGrade(99);
+student1.addGrade(99);
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.present();
+student1.absent();
+student1.absent();
+student1.absent();
+student1.absent();
+student1.absent();
 
 
 
+console.log(student1);
+console.log(`${student1.firstName} is ${student1.getAge()} years old`);
+const averageGrade1 = student1.getAverageGrade();
+console.log(`The average grade of ${student1.firstName} is ${averageGrade1}`);
+console.log(`The average attendance of ${student1.firstName} is  ${student1.averageAttendance()}`);
+console.log(`In total ${student1.firstName}\'s result is ${student1.summary()}`);
 
-const hamburgerLettuce = new Hamburger(SIZE_SMALL, STUFFING_LETTUCE, 'the Hamburger with Lettuce');
-hamburgerLettuce.addTopping(STUFFING_LETTUCE);
-console.log(hamburgerLettuce);
 
-const hamLettuceTP = hamburgerLettuce.calculatePrice();
-console.log(`Total price for ${hamburgerLettuce.name} is ${hamLettuceTP}`);
 
-const hamLettuceCal = hamburgerLettuce.calculateCalories();
-console.log(`Total calories for ${hamburgerLettuce.name} is ${hamLettuceCal}`);
+
+const student2 = new Student('Mika', 'Gallas', 1985);
+student2.addGrade(99);
+student2.addGrade(91);
+student2.addGrade(94);
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.absent();
+student2.present();
+student2.absent();
+console.log(student2);
+console.log(`${student2.firstName} is ${student2.getAge()} years old`);
+const averageGrade2 = student2.getAverageGrade();
+console.log(`The average grade of ${student2.firstName} is ${averageGrade2}`);
+console.log(`The average attendance of ${student2.firstName} is  ${student2.averageAttendance()}`);
+console.log(`In total ${student2.firstName}\'s result is is ${student2.summary()}`);
+
+
+
+const student3 = new Student('Derek', 'Boateng', 1995);
+student3.addGrade(60);
+student3.addGrade(70);
+student3.addGrade(65);
+student3.present();
+student3.absent();
+student3.present();
+student3.absent();
+student3.absent();
+student3.absent();
+student3.absent();
+student3.absent();
+console.log(student3);
+console.log(`${student3.firstName} is ${student3.getAge()} years old`);
+const averageGrade3 = student1.getAverageGrade();
+console.log(`The average grade of ${student3.firstName} is ${averageGrade3}`);
+console.log(`The average attendance of ${student3.firstName} is  ${student3.averageAttendance()}`);
+console.log(`In total ${student3.firstName}\'s result is ${student3.summary()}`);
+
+
+
+const student4 = new Student('Maria', 'Shevchenko', 1993);
+student4.addGrade(80);
+student4.addGrade(50);
+student4.addGrade(60);
+student4.present();
+student4.present();
+student4.present();
+student4.present();
+student4.absent();
+student4.absent();
+student4.present();
+student4.present();
+student4.present();
+student4.present();
+student4.absent();
+console.log(student4);
+console.log(`${student4.firstName} is ${student4.getAge()} years old`);
+const averageGrade4 = student4.getAverageGrade();
+console.log(`The average grade of ${student4.firstName} is ${averageGrade4}`);
+console.log(`The average attendance of ${student4.firstName} is  ${student4.averageAttendance()}`);
+console.log(`In total ${student4.firstName}\'s result is ${student4.summary()}`);
+
+
+
+
+
+
+
+
 
 
