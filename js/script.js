ab8d598b268fdf4dca2c92a6de2707db7ec4e701
Yevhen Larin <minard1usz@gmail.com>
2023-09-05 18:07:52 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,90 +1,52 @@
-const categories = document.querySelectorAll('.category');
-const goodsList = document.querySelector('.goods-list');
-const goodsName = document.querySelector('.goods-name');
-const buyButton = document.querySelector('.buy-btn');
+function displayData() {
+  const firstName = document.getElementById("firstName").value;
+  const secondName = document.getElementById("secondName").value;
+  const birthDate = document.getElementById("birthDate").value;
+  const gender = document.querySelector('input[name="gender"]:checked').value;
+  const city = document.getElementById("city").value;
+  const address = document.getElementById("address").value;
+  const languages = document.querySelectorAll('input[name="languages"]:checked');
 
-const data = {
-  cars: [
-    {name: 'Mazda 6',
-    info: 'Сучасне авто за доступні гроші'},
-    {name: 'BWM e46',
-    info: 'Чудовий вибір, якщо хочеш жити на СТО'},
-    {name: 'Ford Fusion',
-    info: 'Топовий американець, не битий, не крашений'},
-    {name: 'Toyota Camry 3.5',
-    info: 'Неможливо зламати'},
-    {name: 'Renault Logan',
-    info: 'Бюджетний седан'}
-  ],
-  bicycles: [
-    {name: 'PRIDE ROCX 8.2 CF',
-    info: 'Неперевершений гравійник українського бренду'},
-    {name: 'GIANT PROPEL ADVANCED 2',
-    info: 'Топовий шоссер родом із Тайваню'},
-    {name: 'CANNONDALE TRAIL 5 29',
-    info: 'Неймовірний трейловий байк Голландської компанії'},
-    {name: 'MARIN BOBCAT TRAIL 4 29',
-    info: 'Американський кросс-кантрі байк'},
-    {name: 'ORBEA OIZ H30 29',
-    info: 'Гарячий іспанський двопідвіс'}
-  ],
-  motorbikes: [
-    {name: '2016 Harley-Davidson V-Rod Muscle',
-    info: 'Круїзний мотоцикл'},
-    {name: '2022 TORROT Trial Two',
-    info: 'Електромотоцикл'},
-    {name: '2014 Yamaha WR450F',
-    info: 'Дьорт байк'},
-    {name: '2007 Honda VTR250',
-    info: 'Чудовий байк для новачків'},
-    {name: '2016 MV Agusta Brutale 800 Dragster RR',
-    info: 'Шосейний мотоцикл'},
-  ],
-  scooters: [
-    {name: 'Xiaomi Mi Electric Scooter Essential Black',
-    info: 'Бюджетний скутер'},
-    {name: 'Sencor S20 Black',
-    info: 'Скутер для тривалої поїздки'},
-    {name: 'Like.Bike Solo Black',
-    info: 'Розкішний багатофункціональний електросамокат'},
-    {name: 'Segway Ninebot C8 White',
-    info: 'Простий та легкий у користуванні'},
-    {name: 'NAVEE N65 Electric Scooter Black',
-    info: 'Скутер середнього класу для міського використання'}
-  ],
-  rollerblades: [
-    {name: 'Flying Eagle F5S Supersonic Eclipse Black', 
-    info: 'Легкі та швидкі ролики'},
-    {name: 'K2 F.I.T. 80 Pro Grey Black 2021', 
-    info: 'Актуальні на сьогоднішній день'},
-    {name: 'Micro MT Plus Blue', 
-    info: 'Стильні та зручні ролики'},
-    {name: 'Rollerblade Twister Edge Anthracite Yellow 2022', 
-    info: 'Неперевершена якість'},
-    {name: 'Flying Eagle F4 Raven Black', 
-    info: 'Бюджетні та стійкі ролики'}
-  ]
+
+  let listOfLanguages = [];
+  for (const language of languages) {
+    listOfLanguages.push(language.value);
+  }
+
+
+  const outputDiv = document.getElementById("output");
+  outputDiv.innerHTML = `
+  <h2>Реєстраційні дані:</h2>
+  <table>
+      <tr>
+          <td>Ім'я:</td>
+          <td>${firstName}</td>
+      </tr>
+      <tr>
+          <td>Прізвище:</td>
+          <td>${secondName}</td>
+      </tr>
+      <tr>
+          <td>Дата народження:</td>
+          <td>${birthDate}</td>
+      </tr>
+      <tr>
+          <td>Стать:</td>
+          <td>${gender}</td>
+      </tr>
+      <tr>
+          <td>Місто:</td>
+          <td>${city}</td>
+      </tr>
+      <tr>
+          <td>Адреса:</td>
+          <td>${address}</td>
+      </tr>
+      <tr>
+          <td>Мови, якими володіє:</td>
+          <td>${listOfLanguages.join(", ")}</td>
+      </tr>
+  </table>
+  `;
+
 }
-
-categories.forEach(category => {
-  category.addEventListener('click', () => {
-    const selectedCategory = category.getAttribute('data-category');
-    const goods = data[selectedCategory];
-    goodsList.innerHTML = '';
-
-    goods.forEach(goods => {
-      const li = document.createElement('li');
-      li.textContent = goods.name;
-      li.addEventListener('click', () => {
-        goodsName.textContent = `${goods.name} - ${goods.info}`;
-        buyButton.addEventListener('click', () => {
-          li.goods = alert(`Товар "${goods.name}" куплений!`);
-          goodsName.textContent = '';
-          goodsList.textContent = '';
-          buyButton.removeEventListener('click', null);
-        });
-      });
-      goodsList.appendChild(li);
-    });
-  });
-});
