6e7938de104a5992c54c87b19a4637ac16415a2c
Yevhen Larin <minard1usz@gmail.com>
2023-10-11 21:09:56 +0300

    new

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,27 @@
-'use strict';
+const car = { brand: "", model: "", color: "" };
+const updatedProperties = Object.assign(car, {type: ""});
 
-var user = {
-    name: 'John Smith',
-    getName: function() {
-      var self = this;
-      console.log(this.name);
-      console.log(self.name);
-      (function(){
-        console.log(this.name);
-        console.log(self.name);
-      }());
-    }
-  };
-  user.getName();
+const electro = Object.assign({}, car);
+electro.type = "electro";
+
+
+const common = Object.assign({}, car);
+common.type = "common";
+common.gas = "";
+common.gearBox = "";
+
+const models = {
+  tesla: {...electro, brand: "Tesla", model: "S Plaid", maxDistance: 850},
+  leaf: {...electro, brand: "Nissan", model: "Leaf", maxDistance: 270},
+  x5: {...common, brand: "BMW", model: "X5", gas: "Diesel", gearBox: "auto"},
+  kenga: {...common, brand: "Renault", model: "Kangoo", gas: "Petrol", gearBox: "mechanic"}
+};
+
+for (let modelName in models) {
+  console.log(modelName + ":");
+  const model = models[modelName];
+  for (let [key, value] of Object.entries(model)) {
+    console.log(`${key}: ${value}`);
+  }
+  console.log();
+}
